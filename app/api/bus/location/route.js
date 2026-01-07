ae1b00a7a42ba030bd0b625951bc1a78acdd6c86
Hamzaa47 <hamzafaiz635@gmail.com>
2026-01-07 15:41:21 +0500

    Finall

--- app/api/bus/location/route.js
+++ app/api/bus/location/route.js
@@ -3,6 +3,8 @@ import { busStore } from '@/lib/store';
 import { prisma } from '@/lib/prisma';
 import { stops } from '@/lib/data';
 
+export const dynamic = 'force-dynamic';
+
 function getDistanceFromLatLonInMeters(lat1, lon1, lat2, lon2) {
   var R = 6371; // Radius of the earth in km
   var dLat = deg2rad(lat2 - lat1);
